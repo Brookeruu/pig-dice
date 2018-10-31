3c4a39ee7f5520442f311466219c656b984a544e
Brooke Perry and Robert Bruce <brooke@>
2018-10-31 14:10:56 -0700

    add backend functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,55 @@
 function Game() {
   this.players = [],
-    this.currentTurn = this.players[0]
+    this.currentTurn = this.players[0],
+    this.losers = [],
+    this.winner = []
 }
 
 
 Game.prototype.turn = function(player) {
+  console.log("It is " + player.name + "'s turn!!!");
   debugger;
+
   var rollAgain = true;
+  var input = "";
+
   while (rollAgain) {
     player.roll();
-    if (player.rollCheck() === 1) {
+    if (player.rollCheck()) {
+      console.log(player.name + ", Congrats! you rolled a 1!")
       player.rollingScore = 0;
-      player.updateTotalScore;
-      rollAgain = false;
+      console.log(player.name + "'s Total Score: " + player.totalScore)
+      break;
     } else {
       player.rollingScore += player.lastRoll;
+      if (player.totalScore + player.rollingScore >= 10) {
+        console.log("you suck")
+        this.losers.push(player);
+        var indexToRemove = this.players.indexOf(player);
+        this.players.splice(indexToRemove, 1);
+      }
     }
-    var rollAgain = prompt("Would you like to roll again? (true or false only!)")
-    if (rollAgain === false){
-      player.updateTotalScore;
+    input = prompt("Would you like to roll again? (true or false only!)")
+    if (!input) {
+      player.updateTotalScore();
+      console.log(player.name + "'s Current total score: " + player.totalScore)
+      break;
     }
   }
 }
 
-Game.prototype.addPlayer = function(player){
+Game.prototype.addPlayer = function(player) {
   this.players.push(player);
+}
+
+Game.prototype.play = function() {
+  var keepGoing = true;
+  while (keepGoing) {
+    for (var i = 0; i < this.players.length; i++) {
+      this.turn(this.players[i]);
+
+    }
+  }
 }
 
 
@@ -39,19 +64,19 @@ function Player(name) {
 
 Player.prototype.roll = function() {
   var roll = Math.floor(6 * Math.random()) + 1;
-  console.log(this.name + " rolled a: " +roll)
+  console.log(this.name + " rolled a: " + roll)
   this.lastRoll = roll;
 }
 
 Player.prototype.rollCheck = function() {
-    // rollCheck is to check value of roll
-    if (this.lastRoll !== 1) {
-      return true;
-    }
-    // then add value of roll to this.rollingScore)
-    else if (lastRoll === 1) {
-      return false;
-    }
+  // rollCheck is to check value of roll
+  if (this.lastRoll !== 1) {
+    return false;
+  }
+  // then add value of roll to this.rollingScore)
+  else if (this.lastRoll === 1) {
+    return true;
+  }
 }
 
 Player.prototype.addRoll = function() {
@@ -66,41 +91,39 @@ Player.prototype.updateTotalScore = function() {
 }
 
 
-    //
-    // Player.prototype.hold = function() {
-    //
-    // }
+//
+// Player.prototype.hold = function() {
+//
+// }
 
-    // function Cheater(initialScore,name) {
-    //   this.totalScore = initialScore,
-    //   this.rollingScore = 0,
-    //   this.name = name
-    // }
-    //
-    // Cheater.prototype.roll = function() {
-    //
-    // }
-    //
-    // Cheater.prototype.hold = function() {
-    //
-    // }
-    //
+// function Cheater(initialScore,name) {
+//   this.totalScore = initialScore,
+//   this.rollingScore = 0,
+//   this.name = name
+// }
+//
+// Cheater.prototype.roll = function() {
+//
+// }
+//
+// Cheater.prototype.hold = function() {
+//
+// }
+//
 
 
-    // --------------------------------------------------------------------------------
-    var pigDice = new Game();
-    var player1 = new Player("Rob");
-    var player2 = new Player("Brooke");
-    pigDice.addPlayer(player1);
-    pigDice.addPlayer(player2);
-    console.log("Hello " + player1.name + " and " + player2.name);
+// --------------------------------------------------------------------------------
+var pigDice = new Game();
+var player1 = new Player("Rob");
+var player2 = new Player("Brooke");
+pigDice.addPlayer(player1);
+pigDice.addPlayer(player2);
+console.log("Hello " + player1.name + " and " + player2.name);
+// loop will start here
+pigDice.play();
 
-    // loop will start here
-    pigDice.turn(player1);
-    pigDice.turn(player2);
-
-    $(function() {
+$(function() {
 
 
 
-    });
+});
